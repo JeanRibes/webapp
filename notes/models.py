8b3fce1546a01e4f6405b99ed197c23d410f95b9
Jean <jean.christophe.ribes@sfr.fr>
2017-12-12 21:30:34 +0100

    amélioration des models

--- notes/models.py
+++ notes/models.py
@@ -7,6 +7,7 @@ class MatiereModel(models.Model):
         verbose_name=_('Matière')
     nom_matiere = models.CharField(verbose_name=_('Matière'), max_length=255,)
     coefficient = models.DecimalField(verbose_name=_('Coefficient de la matière'), max_digits=4, decimal_places=3, default=1)
+    id = models.IntegerField(verbose_name=_('ID'), primary_key=True)
     def __str__(self):
         return str(self.nom_matiere) + ', coef. ' + str(self.coefficient)
 
@@ -22,13 +23,13 @@ class NotesModel(models.Model):
     class Meta:
         verbose_name_plural = _('notes')
         verbose_name = _('note')
-    #matiere = models.CharField(verbose_name=_('Matière'), max_length=30)
     matiere = models.ForeignKey(MatiereModel, on_delete=models.CASCADE, verbose_name=_('Matière'))
     nom = models.CharField(verbose_name=_('Nom du contrôle'), max_length=255, default='IE')
     desc = models.CharField(verbose_name=_('Description du contrôle'), max_length=255, blank=True)
-    date = models.DateTimeField(verbose_name=_('Date du contrôle'))
+    date = models.DateField(verbose_name=_('Date du contrôle'))
     note = models.IntegerField(verbose_name=_('Note /20'))
     coef = models.DecimalField(verbose_name=_('Coefficient'), max_digits=4, decimal_places=3, default=1)
     typenote = models.ForeignKey(TypeNote, on_delete=models.CASCADE, verbose_name=_('Type de note'))
+    id = models.IntegerField(verbose_name=_('ID'), primary_key=True)
     def __str__(self):
         return str(self.nom) + ", " + str(self.note) + "/20"
--- notes/views.py
+++ notes/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from notes.models import NotesModel
 # Create your views here.
 def moyenne(request):
-    Notes = NotesModel.objects.get.all()
-#    notes = Notes.note
+    Notes = NotesModel.objects.get('all')
+    notes = Notes.note
     somme = 0
     for i in notes:
         somme += notes[i]
